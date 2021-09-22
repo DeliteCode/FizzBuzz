2fb0ed2d6b89fa0381b3b633ffcb669646ab2869
DeliteCode <80368625+DeliteCode@users.noreply.github.com>
2021-09-22 00:02:47 -0300

    fizzBuzz 22 setembro

--- js/site.js
+++ js/site.js
@@ -1,4 +1,70 @@
-function helloWorld() {
-    alert("Hello World!");
+//get values from the user. We need to get the fizz and buzz value
+function getValues() {
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
+
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        let fbArray = fizzBuzz(fizzValue, buzzValue);
+        displayData(fbArray);
+    } else {
+        alert("You must enter an integer"); 
+    }
+
+}
+
+function fizzBuzz(fizzValue, buzzValue) {
+
+    let returnArray = [];
+    for (let i = 1; i < 100; i++) {
+        if (i % fizzValue == 0 && i % buzzValue == 0) {
+            returnArray.push('FizzBuzz');        
+        } else if(i % fizzValue == 0 ) {
+            returnArray.push('Fizz');        
+        } 
+        else if (i % buzzValue == 0 ) {
+            returnArray.push('Buzz');        
+        } 
+        else {
+            returnArray.push(i);        
+        }
+
+    }
+
+    return returnArray;
+}
+
+function displayData(fbArray) {
+
+    let tableBody = document.getElementById("results");
+
+    let templateRow = document.getElementById("fbTemplate");
+
+    tableBody.innerHTML = "";
+
+    for (let index = 0; index < fbArray.length; index+=5) {
+        let tableRow = document.importNode(templateRow.content, true);        
+  
+        let rowCols = tableRow.querySelectorAll("td");   
+        rowCols[0].classList.add(fbArray[index]);
+        rowCols[0].textContent = fbData[index]; 
+
+        rowCols[1].classList.add(fbArray[index + 1]);  
+        rowCols[1].textContent = fbData[index+1]; 
+
+        rowCols[2].classList.add(fbArray[index + 2]);
+        rowCols[2].textContent = fbData[index+2]; 
+
+        rowCols[3].classList.add(fbArray[index +3]);
+        rowCols[3].textContent = fbData[index+3]; 
+
+        rowCols[4].classList.add(fbArray[index + 4]);
+        rowCols[4].textContent = fbData[index+4]; 
+
+        tableBody.appendChild(tableRow);
+
+    }
 
 }
